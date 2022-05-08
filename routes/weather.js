de2f5c47f2171ac4423e9822a2620cc53539066e
santhosh <santhoshsanthosh63268@gmail.com>
2022-05-08 21:28:38 +0530

    Update app

--- routes/weather.js
+++ routes/weather.js
@@ -2,17 +2,66 @@ require("dotenv").config();
 const router = require("express").Router();
 const fetch = require("node-fetch");
 
-router.get("/", (req, res) => {
-  res.render("index",{
-    city:null,
-    des:null,
-    icon:null,
-    temp:null,
-    temp_min:null,
-    temp_max:null,
-    speed:null,
-    humidity:null
-  });
+router.get("/", async (req, res) => {
+  // res.render("index",{
+  //   city:null,
+  //   des:null,
+  //   icon:null,
+  //   temp:null,
+  //   temp_min:null,
+  //   temp_max:null,
+  //   speed:null,
+  //   humidity:null
+  // });
+
+  const url_api = `https://api.openweathermap.org/data/2.5/weather?q=Coimbatore&appid=${process.env.API_KEY}`;
+
+  try {
+    await fetch(url_api)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.message === "city not found") {
+          res.render("index", {
+            city: data.message,
+            des: null,
+            icon: null,
+            temp: null,
+            temp_min: null,
+            temp_max: null,
+            speed: null,
+            humidity: null,
+          });
+        } else {
+          const city = data.name;
+          const des = data.weather[0].description;
+          const temp = data.main.temp;
+          const temp_min = data.main.temp_min;
+          const temp_max = data.main.temp_max;
+          const speed = data.wind.speed;
+          const humidity = data.main.humidity;
+
+          res.render("index", {
+            city,
+            des,
+            temp,
+            temp_min,
+            temp_max,
+            speed,
+            humidity,
+          });
+        }
+      });
+  } catch (err) {
+    res.render("index", {
+      city: "something went wrong",
+      des: null,
+      temp: null,
+      temp_min: null,
+      temp_max: null,
+      speed: null,
+      humidity: null,
+    });
+  }
 });
 
 // display on the command
@@ -27,43 +76,105 @@ router.post("/", async (req, res) => {
 
   try {
     await fetch(url_api)
-     .then(res => res.json())
-     .then(data =>{
-         if (data.message === 'city not found') {
-             res.render('index',{
-                 city:data.message,
-                 des:null,
-                 icon:null,
-                 temp:null,
-                 temp_min:null,
-                 temp_max:null,
-                 speed:null,
-                 humidity:null
-             })   
-         }else{
-             const city =data.name;
-             const des = data.weather[0].description;
-             const temp =data.main.temp;
-             const temp_min =data.main.temp_min;
-             const temp_max =data.main.temp_max;
-             const speed = data.wind.speed;
-             const humidity = data.main.humidity;
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.message === "city not found") {
+          res.render("index", {
+            city: data.message,
+            des: null,
+            icon: null,
+            temp: null,
+            temp_min: null,
+            temp_max: null,
+            speed: null,
+            humidity: null,
+          });
+        } else {
+          const city = data.name;
+          const des = data.weather[0].description;
+          const temp = data.main.temp;
+          const temp_min = data.main.temp_min;
+          const temp_max = data.main.temp_max;
+          const speed = data.wind.speed;
+          const humidity = data.main.humidity;
 
-             res.render('index',{
-                 city,des,temp,temp_min,temp_max,speed,humidity
-                 });
-         }
-     }); 
+          res.render("index", {
+            city,
+            des,
+            temp,
+            temp_min,
+            temp_max,
+            speed,
+            humidity,
+          });
+        }
+      });
   } catch (err) {
-      res.render('index',{
-          city:'something went wrong',
-          des:null,
-          temp:null,
-          temp_min:null,
-          temp_max:null,
-          speed:null,
-          humidity:null
-      })
+    res.render("index", {
+      city: "something went wrong",
+      des: null,
+      temp: null,
+      temp_min: null,
+      temp_max: null,
+      speed: null,
+      humidity: null,
+    });
+  }
+});
+
+router.get("/:city", async (req, res) => {
+  const city = req.params.city;
+
+  const url_api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+
+  const response = await fetch(url_api);
+  const body = await response.text();
+
+  try {
+    await fetch(url_api)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.message === "city not found") {
+          res.render("index", {
+            city: data.message,
+            des: null,
+            icon: null,
+            temp: null,
+            temp_min: null,
+            temp_max: null,
+            speed: null,
+            humidity: null,
+          });
+        } else {
+          const city = data.name;
+          const des = data.weather[0].description;
+          const temp = data.main.temp;
+          const temp_min = data.main.temp_min;
+          const temp_max = data.main.temp_max;
+          const speed = data.wind.speed;
+          const humidity = data.main.humidity;
+
+          res.render("index", {
+            city,
+            des,
+            temp,
+            temp_min,
+            temp_max,
+            speed,
+            humidity,
+          });
+        }
+      });
+  } catch (err) {
+    res.render("index", {
+      city: "something went wrong",
+      des: null,
+      temp: null,
+      temp_min: null,
+      temp_max: null,
+      speed: null,
+      humidity: null,
+    });
   }
 });
 
